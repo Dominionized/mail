dabccebb657335a041d5bfe56e53aafbd88f1dd7
dominionized <dominique.begin.0@gmail.com>
2015-03-06 11:51:49 -0500

    Changed dots position:

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var mail = angular.module('mail', ['ngRoute', 'inbox']);
 
 mail.config(['$routeProvider',
 	function($routeProvider){
-		$routeProvider.
-			when('/', {
+		$routeProvider
+			.when('/', {
 				templateUrl: 'inbox/inbox.tpl.html',
 				controller: 'InboxCtrl'
-			}).
-			otherwise({
+			})
+			.when('/prefs', {
+				templateUrl: 'prefs/prefs.tpl.html',
+				controller: 'PrefsCtrl'
+			})
+			.otherwise({
 				redirectTo: '/'
 			});
 	}]);
